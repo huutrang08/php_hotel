d821f617d268dc7d346ce801b21d7bb48cea351f
huutrang08 <buihuutrang2110@gmail.com>
2022-04-24 18:12:13 +0700

    first

--- admin/reservation.php
+++ admin/reservation.php
@@ -6,7 +6,7 @@ include('db.php')
 <head>
       <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>RESERVATION SUNRISE HOTEL</title>
+    <title>ĐẶT PHÒNG LUXURY HOTEL</title>
 	<!-- Bootstrap Styles-->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
      <!-- FontAwesome Styles-->
@@ -23,7 +23,7 @@ include('db.php')
                 <ul class="nav" id="main-menu">
 
                     <li>
-                        <a  href="../index.php"><i class="fa fa-home"></i> Homepage</a>
+                        <a  href="../index.php"><i class="fa fa-home"></i> Trang Chủ</a>
                     </li>
                     
 					</ul>
@@ -37,7 +37,7 @@ include('db.php')
 			 <div class="row">
                     <div class="col-md-12">
                         <h1 class="page-header">
-                            RESERVATION <small></small>
+                            Đặt Phòng <small></small>
                         </h1>
                     </div>
                 </div> 
@@ -48,7 +48,7 @@ include('db.php')
                 <div class="col-md-5 col-sm-5">
                     <div class="panel panel-primary">
                         <div class="panel-heading">
-                            PERSONAL INFORMATION
+                            Thông Tin Cá Nhân
                         </div>
                         <div class="panel-body">
 						<form name="form" method="post">
@@ -66,12 +66,12 @@ include('db.php')
                                             </select>
                               </div>
 							  <div class="form-group">
-                                            <label>First Name</label>
+                                            <label>Họ</label>
                                             <input name="fname" class="form-control" required>
                                             
                                </div>
 							   <div class="form-group">
-                                            <label>Last Name</label>
+                                            <label>Tên</label>
                                             <input name="lname" class="form-control" required>
                                             
                                </div>
@@ -81,7 +81,7 @@ include('db.php')
                                             
                                </div>
 							   <div class="form-group">
-                                            <label>Nationality*</label>
+                                            <label>Quốc Gia</label>
                                             <label class="radio-inline">
                                                 <input type="radio" name="nation"  value="Sri Lankan" checked="">Sri Lankan
                                             </label>
@@ -96,7 +96,7 @@ include('db.php')
 
 								?>
 								<div class="form-group">
-                                            <label>Passport Country*</label>
+                                            <label>Quốc Tịch*</label>
                                             <select name="country" class="form-control" required>
 												<option value selected ></option>
                                                 <?php
@@ -107,7 +107,7 @@ include('db.php')
                                             </select>
 								</div>
 								<div class="form-group">
-                                            <label>Phone Number</label>
+                                            <label>SĐT</label>
                                             <input name="phone" type ="text" class="form-control" required>
                                             
                                </div>
@@ -122,27 +122,27 @@ include('db.php')
                 <div class="col-md-6 col-sm-6">
                     <div class="panel panel-primary">
                         <div class="panel-heading">
-                            RESERVATION INFORMATION
+                            Thông Tin Đặt Phòng
                         </div>
                         <div class="panel-body">
 								<div class="form-group">
-                                            <label>Type Of Room *</label>
+                                            <label>Loại Phòng*</label>
                                             <select name="troom"  class="form-control" required>
 												<option value selected ></option>
-                                                <option value="Superior Room">SUPERIOR ROOM</option>
-                                                <option value="Deluxe Room">DELUXE ROOM</option>
-												<option value="Guest House">GUEST HOUSE</option>
-												<option value="Single Room">SINGLE ROOM</option>
+                                                <option value="Superior Room">Phòng Đôi</option>
+                                                <option value="Deluxe Room">Phòng Víp</option>
+												<option value="Guest House">Phòng Thiên Nhiên</option>
+												<option value="Single Room">Phòng Đơn</option>
                                             </select>
                               </div>
 							  <div class="form-group">
-                                            <label>Bedding Type</label>
+                                            <label>Loại Phòng Ngủ</label>
                                             <select name="bed" class="form-control" required>
 												<option value selected ></option>
-                                                <option value="Single">Single</option>
-                                                <option value="Double">Double</option>
-												<option value="Triple">Triple</option>
-                                                <option value="Quad">Quad</option>
+                                                <option value="Single">Đơn</option>
+                                                <option value="Double">Đôi</option>
+												<option value="Triple">Ba</option>
+                                                <option value="Quad">Bốn</option>
 												<option value="None">None</option>
                                                 
                                              
@@ -164,13 +164,13 @@ include('db.php')
 							 
 							 
 							  <div class="form-group">
-                                            <label>Meal Plan</label>
+                                            <label>Kế Hoạch Ăn</label>
                                             <select name="meal" class="form-control"required>
 												<option value selected ></option>
-                                                <option value="Room only">Room only</option>
-                                                <option value="Breakfast">Breakfast</option>
-												<option value="Half Board">Half Board</option>
-												<option value="Full Board">Full Board</option>
+                                                <option value="Room only">Chỉ Đặt Phòng</option>
+                                                <option value="Breakfast">Ăn Sáng</option>
+												<option value="Half Board">Ăn nhẹ</option>
+												<option value="Full Board">Ăn Cả Ngày</option>
 												
                                                 
                                              
@@ -194,9 +194,9 @@ include('db.php')
 				
                 <div class="col-md-12 col-sm-12">
                     <div class="well">
-                        <h4>HUMAN VERIFICATION</h4>
-                        <p>Type Below this code <?php $Random_code=rand(); echo$Random_code; ?> </p><br />
-						<p>Enter the random code<br /></p>
+                        <h4>Xác Thực</h4>
+                        <p>Nhập Code Vào bên dưới <?php $Random_code=rand(); echo$Random_code; ?> </p><br />
+						<p>Nhập<br /></p>
 							<input  type="text" name="code1" title="random code" />
 							<input type="hidden" name="code" value="<?php echo $Random_code; ?>" />
 						<input type="submit" name="submit" class="btn btn-primary">
